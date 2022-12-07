cad001f9a56fc6a3f612d222e059b2e2e35e2877
z9877969 <z9877969@ukr.net>
2022-12-07 23:42:33 +0200

    fixes warning

--- src/hooks/useToastError.js
+++ src/hooks/useToastError.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
